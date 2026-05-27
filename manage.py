b1704c79bc24b4707e4f8cd2ad4f5081524145b1
Volodymyr Yalechko <Volodymyr.I.Yalechko@lpnu.ua>
2026-05-27 18:57:56 +0300

    fix username in manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,17 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pet_shelter_project.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pet_shelter_project.settings')
+
+    if 'RENDER' in os.environ:
+        import django
+        django.setup()
+        from django.contrib.auth import get_user_model
+        user_model = get_user_model()
+        if not user_model.objects.filter(username='user').exists():
+            user_model.objects.create_superuser('user', 'user@admin.com', 'user12345')
+            print("=== Superuser 'user' created successfully! ===")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +26,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
